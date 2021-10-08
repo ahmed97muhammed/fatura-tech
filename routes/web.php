c741f99facb8ddef4071a6d69ca26e97324275b6
Ahmed Muhammed <86477530+ahmed97muhammed@users.noreply.github.com>
2021-10-08 13:33:24 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,3 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/systemusers', 'UserController@systemusers');
-
-Route::get('get_system_users_info','UserController@get_system_users_info');
-
-Route::post('deleteuser_ajax_url','UserController@deleteuser_ajax_url');
-
-Route::post('update_permision_ajax','UserController@update_permision_ajax');
